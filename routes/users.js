ff7c8384da1ff915c395677b64756dfeaf3eeef6
savvyscript <nm3142@hotmail.com>
2017-03-27 17:19:36 -0400

    more updates

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,87 @@
 var express = require('express');
 var router = express.Router();
 
-var Author = require('../models/author');
+var Users = require('../models/users');
 
 // index authors
 router.get('/', function(req, res) {
     // res.send('authors will be here');
     Author.find({})
-        .exec(function(err, authors) {
+        .exec(function(err, users) {
             if(err) console.log(err);
 
-            console.log(authors);
-            res.send(authors);
+            console.log(users);
+            res.render('users/index', {
+                  users: users
+            });
         });
 });
 
+// new user
+router.get('/new', function(req, res) {
+    res.render('users/new');
+});
+
+// create user
+router.post('/', function(req, res) {
+    var user = new User({
+        first_name: req.body.first_name,
+	    last_name: req.body.last_name,
+	    email: req.body.email,
+	    username: req.body.username,
+	    password: req.body.password,
+    });
+    user.save(function(err, user){
+        if (err) { console.log(err); }
+
+        console.log(users);
+        res.render('users/show', {
+            author: author
+        });
+    });
+});
+
+// show author
 router.get('/:id', function(req, res) {
-    Author.findById(req.params.id)
-        .exec(function(err, author) {
+    users.findById(req.params.id)
+        .exec(function(err, users) {
             if(err) console.log(err);
 
+            console.log(users);
+            // res.send(users);
+            res.render('users/show', {
+                users: users
+            });
+        });
+});
+
+// edit users
+router.get('/:id/edit', function(req,res) {
+    Users.findById(req.params.id)
+    .exec(function(err, users) {
+        if (err) { console.log(err); }
+
+        res.render('users/edit', {
+            users: users
+        });
+    });
+});
+
+// update users
+router.patch('/:id', function(req, res) {
+    Users.findByIdAndUpdate(req.params.id, {
+        first_name: req.body.first_name,
+	    last_name: req.body.last_name,
+	    email: req.body.email,
+	    username: req.body.username,
+	    password: req.body.password,
+    }, {new: true})
+        .exec(function(err, users) {
+            if (err) { console.log(err); }
+
             console.log(author);
-            // res.send(author);
-            res.render('authors/show', {
-                author: author
+            res.render('users/show', {
+                users: users
             });
         });
 });
